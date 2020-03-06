d0d81ff14404fbe474dc09fcb15512bf85023458
Calegari <calegari2711@gmail.com>
2020-03-06 15:22:52 -0300

    Update real

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { LivrosListaComponent } from './livros/livros-lista/livros-lista.component';
 import { LivrosInformacoesComponent } from './livros/livros-informacoes/livros-informacoes.component';
-=======
 import { MenuComponent } from './menu/menu.component';
 import { HomeComponent } from './home/home.component';
->>>>>>> 7b739c1263eebff487b148ea898f0675f3880846
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     LivrosListaComponent,
-    LivrosInformacoesComponent
-=======
+    LivrosInformacoesComponent,
     MenuComponent,
     HomeComponent
->>>>>>> 7b739c1263eebff487b148ea898f0675f3880846
   ],
   imports: [
     BrowserModule,
