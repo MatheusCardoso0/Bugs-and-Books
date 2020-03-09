95e55682ce190634bc91218e242c2ef1c1d1c94e
MatheusCardoso0 <mathde12@gmail.com>
2020-03-09 08:45:18 -0300

    SLA

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { HomeComponent } from './home/home.component';
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LivrosListaComponent } from './livros/livros-lista/livros-lista.component';
+import { LivrosInformacoesComponent } from './livros/livros-informacoes/livros-informacoes.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent }
+  // { path: '', component: LivrosListaComponent },
+  // { path: '', component: LivrosInformacoesComponent }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
